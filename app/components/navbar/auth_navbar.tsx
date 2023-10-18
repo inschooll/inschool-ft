21a0c3d97f24c2306a8ef7dd90d81db95daeb84d
dc <00danzy@gmail.com>
2023-10-18 23:04:32 +0100

    complete navbar

--- app/components/navbar/auth_navbar.tsx
+++ app/components/navbar/auth_navbar.tsx
@@ -1,28 +1,78 @@
+"use client";
+
 import Image from "next/image";
-import constants from "../../constants/constants";
+import constants from "../../core/constants/constants";
 import Button from "../buttons/button";
+import Link from "next/link";
+import { useState } from "react";
+import links from "@/app/core/constants/links";
+import MenuCancelButton from "../buttons/menu_cancel_button";
 
 const AuthNavbar = () => {
+  var [showMenuDropdown, setShowMenuDropdown] = useState(false);
+
+  const handleMenuClick = (value: boolean) => {
+    setShowMenuDropdown(value);
+  };
+
   return (
     <nav>
-      <div className="flex justify-between items-center px-5 py-3">
+      <div className="flex justify-between items-center mx-5 sm:mx-10 py-5">
         {/* logo */}
-        <Image
-          src={constants.logoFull}
-          alt="inschool logo"
-          width={150}
-          height={40}
-        />
+        <Link href={links.landingPage}>
+          <Image
+            src={constants.logoFull}
+            alt="inschool logo"
+            width={150}
+            height={40}
+          />
+        </Link>
 
         {/* links / buttons */}
-        <div className="flex gap-1">
-          {/* <Button bgColor={"red-500"} textColor={""} hoverBgColor={""} fontSize={""} fontWeight={""}>Log in</Button>
-          <Button bgColor={"green-700"} textColor={""} hoverBgColor={""} fontSize={""} fontWeight={""}>Sign up</Button> */}
-          <Button>Log in</Button>
+        <div className="hidden lg:flex gap-2">
+          <Button variant={"noBackground"} href="/auth/login">
+            Log in
+          </Button>
+          <Button variant={"default"} href="/auth/signup" className="px-10">
+            Sign up
+          </Button>
         </div>
+
+        <div className="lg:hidden">
+          <MenuCancelButton showMenu={showMenuDropdown} onClick={() => handleMenuClick(!showMenuDropdown)} />
+        </div>
+
+        {showMenuDropdown && <DropdownMenu />}
       </div>
     </nav>
   );
 };
 
 export default AuthNavbar;
+
+const DropdownMenu = () => {
+  return (
+    <div className="block lg:hidden w absolute top-[76px] z-10 left-0 w-full h-4/5 bg-white border-b border-gray-200">
+      <div className="flex justify-stretch flex-col mx-5 sm:mx-10 py-7">
+        <Button
+          variant={"outlineFull"}
+          size={"sm"}
+          className="block"
+          href={links.login}
+        >
+          Log in
+        </Button>
+        <Button
+          variant={"defaultFull"}
+          size={"sm"}
+          className="mt-2 block"
+          href={links.signup}
+        >
+          Sign up
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+
