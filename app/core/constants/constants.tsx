15ca90a3b2b0a37add85af4b3f4b8df5aeadc0b2
dc <00danzy@gmail.com>
2023-10-18 23:01:46 +0100

    add doubleArrowUp, menu, cancel, googleLogo icons

--- app/core/constants/constants.tsx
+++ app/core/constants/constants.tsx
@@ -2,7 +2,10 @@ const logoFull = '/logo/logo-full.svg';
 const brainPink = '/logo/brain-pink.svg';
 const bookStack = '/logo/book-stack.svg';
 const mathsCalculation = '/images/maths-calculations.png';
-const doubleArrowUp = '/images/double-arrow-up.svg';
+const doubleArrowUp = '/icons/double-arrow-up.svg';
+const menu = '/icons/menu.svg';
+const cancel = '/icons/cancel.svg';
+const googleLogo = '/icons/google-logo.svg';
 
 
 export default {
