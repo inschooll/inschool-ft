fe39524f97ccbbe1689491cedfdac1194b1e3f3f
dc <00danzy@gmail.com>
2023-10-18 23:03:12 +0100

    improve button

--- app/components/buttons/button.tsx
+++ app/components/buttons/button.tsx
@@ -5,63 +5,67 @@ import React from "react";
 
 const buttonVariants = cva(
   // 'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 dark:hover:bg-slate-800 dark:hover:text-slate-100 disabled:opacity-50 dark:focus:ring-slate-400 disabled:pointer-events-none dark:focus:ring-offset-slate-900 data-[state=open]:bg-slate-100 dark:data-[state=open]:bg-slate-800',
-  'bg-black text-white hover:bg-gray-900 px-7 py-1 text-sm rounded-sm font-semibold',
+  "bg-[#2d2d2d] text-white shadow hover:shadow-md hover:bg-[#121212] active:bg-[#2d2d2d] px-7 py-1 text-sm rounded-[4px] font-semibold transition duration-[500] whitespace-nowrap",
   {
     variants: {
       variant: {
-        default:
-          'bg-black text-white hover:bg-gray-900 px-7 py-1 text-sm rounded-sm font-semibold',
-        destructive:
-          'bg-red-500 text-white hover:bg-red-600 dark:hover:bg-red-600',
-        outline:
-          'bg-transparent border border-slate-200 hover:bg-slate-100 dark:border-slate-700 dark:text-slate-100',
-        subtle:
-          'bg-slate-100 text-slate-900 hover:bg-slate-200 dark:bg-slate-700 dark:text-slate-100',
-        ghost:
-          'bg-transparent dark:bg-transparent hover:bg-slate-100 dark:hover:bg-slate-800 dark:text-slate-100 dark:hover:text-slate-100 data-[state=open]:bg-transparent dark:data-[state=open]:bg-transparent',
-        link: 'bg-transparent dark:bg-transparent underline-offset-4 hover:underline text-slate-900 dark:text-slate-300 hover:bg-transparent dark:hover:bg-transparent',
+        default: "text-white px-7 py-1 text-sm rounded-[4px] font-semibold",
+        noBackground: "bg-transparent text-black hover:bg-gray-100 active:bg-transparent",
+        outline: "bg-transparent border border-gray-100 text-black hover:bg-gray-100 active:bg-transparent",
+
+        defaultFull: "text-white px-7 py-1 w-full text-sm rounded-[4px] font-semibold",
+        noBackgroundFull: "bg-transparent w-full text-black hover:bg-gray-100 active:bg-[#2d2d2d]",
+        outlineFull: "bg-transparent border w-full border-gray-100 text-black hover:bg-gray-100 active:bg-transparent",
+
       },
       size: {
-        default: 'h-10 py-1 px-7',
-        sm: 'h-9 px-2 rounded-md',
-        lg: 'h-11 px-8 rounded-md',
+        default: "py-1 px-7",
+        sm: "py-2",
+        md: "py-3",
+        lg: "py-4",
       },
     },
     defaultVariants: {
-      variant: 'default',
-      size: 'default',
+      variant: "default",
+      size: "default",
     },
   }
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  href?: string
+  href?: string;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({ className, children, href, variant, size, ...props }, ref) => {
-  if (href) {
-    return (
-      <Link
-        href={href}
-        className={cn(buttonVariants({ variant, size, className }))}
-      >
-        {children}
-      </Link>
-    )
+const LinkTemplate = ({
+  children,
+  href,
+}: {
+  children: React.ReactNode;
+  href?: string;
+}) => {
+  if (href != null) {
+    return <Link href={href}>{children}</Link>;
+  } else {
+    return children;
   }
+};
 
-  return (
-    <button
-      className={cn(buttonVariants({ variant, size, className }))}
-      ref={ref}
-      {...props}
-    >
-      {children}
-    </button>
-  );
-}); 
-
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, href, variant, size, ...props }, ref) => {
+    return (
+      <LinkTemplate href={href}>
+        <button
+          className={cn(buttonVariants({ variant, size, className }))}
+          ref={ref}
+          {...props}
+        >
+          {children}
+        </button>
+      </LinkTemplate>
+    );
+  }
+);
 
 export default Button;
