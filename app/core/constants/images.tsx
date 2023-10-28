14f978f3d9bb4937ae42abbd935b23a4074d9039
dc <00danzy@gmail.com>
2023-10-28 16:29:08 +0100

    first commit

---
+++ app/core/constants/images.tsx
@@ -0,0 +1,34 @@
+const favicon = 'favicon.png';
+const logo = '/logo/logo.svg';
+const logoFull = '/logo/logo-full.svg';
+const brainPink = '/logo/brain-pink.svg';
+const bookStack = '/logo/book-stack.svg';
+const mathsCalculations = '/images/maths-calculations.svg';
+const mathsCalculation = '/images/maths-calculation.svg';
+const doubleArrowUp = '/icons/double-arrow-up.svg';
+const menu = '/icons/menu.svg';
+const cancel = '/icons/cancel.svg';
+const googleLogo = '/icons/google-logo.svg';
+const nigeriaFlag = '/icons/countries/nigeria.svg';
+const downArrow = '/icons/down-arrow.svg';
+const schoolLogo = '/images/schools/bingham.png';
+
+const random1 = '/images/random-1.jpg';
+
+export default {
+  favicon,
+  logo,
+  logoFull,
+  brainPink,
+  bookStack,
+  mathsCalculations,
+  mathsCalculation,
+  doubleArrowUp,
+  menu,
+  cancel,
+  googleLogo,
+  nigeriaFlag,
+  downArrow,
+  schoolLogo,
+  random1,
+};
